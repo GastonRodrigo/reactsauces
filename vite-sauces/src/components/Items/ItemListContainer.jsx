49a64f91005910c655e584ae256d8beb532648ca
GastonRodrigo <cordoba.gastonr@yahoo.com>
2023-09-21 23:34:34 -0300

    fix itemlistcontainer

--- vite-sauces/src/components/Items/ItemListContainer.jsx
+++ vite-sauces/src/components/Items/ItemListContainer.jsx
@@ -1,11 +1,9 @@
-import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Box from "@mui/material/Box";
-import { Button } from "bootstrap";
 
 const ItemListContainer = ({name, img, description, price}) => {
   return (
